d46042400cbf60c7c48220c37e4fce479bec25f5
Lucas Bicca <lucaslbs15@gmail.com>
2017-11-26 19:01:29 -0200

    Adding Fuel Injection Timing command.

--- src/main/java/com/github/pires/obd/enums/AvailableCommandNames.java
+++ src/main/java/com/github/pires/obd/enums/AvailableCommandNames.java
@@ -57,7 +57,8 @@ public enum AvailableCommandNames {
     WIDEBAND_AIR_FUEL_RATIO("Wideband Air/Fuel Ratio"),
     DESCRIBE_PROTOCOL("Describe protocol"),
     DESCRIBE_PROTOCOL_NUMBER("Describe protocol number"),
-    IGNITION_MONITOR("Ignition monitor")
+    IGNITION_MONITOR("Ignition monitor"),
+    FUEL_INJECTION_TIMING("Fuel Injection Timing")
     ;
 
     private final String value;
